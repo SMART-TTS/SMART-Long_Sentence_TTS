0a2dbdd28105c40a390524d86ebbd49e83c4cbf4
shjang-97 <shjang97@hanyang.ac.kr>
2020-11-20 18:58:42 +0900

    developing...

--- model.py
+++ model.py
@@ -583,9 +583,7 @@ class Tacotron2(nn.Module):
         encoder_outputs = self.encoder(embedded_inputs, text_lengths)
         length_regulator_output, duration = self.length_regulator(encoder_outputs, target=alignments_padded)
         # if alignments_padded is not None:
-        #     length_regulator_output, duration =
 
- 
         mel_outputs, gate_outputs, alignments = self.decoder(
             encoder_outputs, mels, output_lengths, memory_lengths=text_lengths)
 
